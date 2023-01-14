066551b31a0e2d66d86513db2e1c24d85db1d558
TyfangXV <9fangdev@gmail.com>
2023-01-14 16:51:39 +0400

    Publish

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const require = createRequire(import.meta.url);
 import fs from "fs";
 import clear from "clear";
 import inquirer from "inquirer";
-import AWS from 'aws-sdk'
+import ibm from 'ibm-cos-sdk'
 import path from 'path'
 
 // ##############################--modules---#######################
@@ -15,7 +15,7 @@ const getCurrentDirectoryBase = () => {
 
 const configs = () => {
   const dir = getCurrentDirectoryBase();
-  if(fs.existsSync(`${dir}/dosnic.config.json`))
+  if(fs.existsSync(`${dir}/.dosnic.config.json`))
   {
   }else{
     console.log(chalk.red("No config found"));
@@ -26,16 +26,17 @@ const configs = () => {
 
 
 //  ########################---config--######################
-const importPath = `${getCurrentDirectoryBase()}/dosnic.config.json`;
+const importPath = `${getCurrentDirectoryBase()}/.dosnic.config.json`;
 
 const config = require(importPath);
-
 // Replace with your own access key ID and secret access key
-AWS.config.update({
-    accessKeyId: config.access_id,
-  });
-  
-const s3 = new AWS.S3();
+var creds = {
+  endpoint: config.endpoint,
+  apiKeyId: config.api_key,
+  serviceInstanceId: config.service_id,
+};
+
+var cos = new ibm.S3(creds);
 
 
 //  ########################---code--######################
@@ -65,22 +66,18 @@ const locator = () => {
 
 
 //upload the file to the cloud storage 
-const uploadFile = (filePath, bucket, key) => {
-    const fileContent = fs.readFileSync(filePath);
-  
-    const params = {
-      Bucket: bucket,
-      Key: key,
-      Body: fileContent,
-    };
-  
-    s3.upload(params, (err, data) => {
-      if (err) {
-        console.error(`Error: ${err}`);
-      } else {
-        console.log(`Successfully uploaded ${data.Key} to ${data.Bucket}`);
-      }
-    });
+const uploadFile = (filePath) => {
+  try {
+      const fileContent = fs.readFileSync(filePath);
+      return cos.putObject({
+        Bucket: config.bucket,
+        Key : config.key_name,
+        Body: fileContent
+    })
+  } catch (error) {
+      console.log(error);
+      process.exit();
+  }
 };
 
 
@@ -90,9 +87,15 @@ const main = async() => {
     console.log(chalk.yellow(figlet.textSync("DOSNIC DEV", {horizontalLayout : true})));
     configs();
     const {filePath} = await locator();
-    console.log(AWS.config.acm);
     console.log(chalk.greenBright("Uploading..."));
 
+    const uploadLog = uploadFile(filePath)
+                                  .send((err) => {
+                                      if(err) console.log(chalk.redBright(err));
+                                      if(!err) console.log(chalk.yellowBright("File has been uploaded"));
+                                  });
+
+    console.log(chalk.greenBright("Uploaded"));
 
 }
 
