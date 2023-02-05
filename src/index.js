da15505ed9de61922db7ed7ea06b69db6b2807cb
TyfangXV <9fangdev@gmail.com>
2023-02-05 14:32:03 +0400

    Newly build cli

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+#!/usr/bin/env node
+
+import xmlParse from 'xml2js'
 import chalk from "chalk";
 import figlet from "figlet";
 import { createRequire } from "module";
@@ -7,6 +10,7 @@ import clear from "clear";
 import inquirer from "inquirer";
 import ibm from 'ibm-cos-sdk'
 import path from 'path'
+import axios from 'axios';
 
 // ##############################--modules---#######################
 const getCurrentDirectoryBase = () => {
@@ -37,7 +41,15 @@ var creds = {
 };
 
 var cos = new ibm.S3(creds);
-
+function create_UUID(){
+  var dt = new Date().getTime();
+  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = (dt + Math.random()*16)%16 | 0;
+      dt = Math.floor(dt/16);
+      return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+  });
+  return uuid;
+}
 
 //  ########################---code--######################
 
@@ -65,13 +77,15 @@ const locator = () => {
 }
 
 
+
 //upload the file to the cloud storage 
-const uploadFile = (filePath) => {
+const uploadFile = (filePath, file_name) => {
   try {
+      if(filePath.includes("\\")) filePath.replaceAll("\\", "/")
       const fileContent = fs.readFileSync(filePath);
       return cos.putObject({
         Bucket: config.bucket,
-        Key : config.key_name,
+        Key : file_name || filePath.split("/").pop(),
         Body: fileContent
     })
   } catch (error) {
@@ -83,19 +97,114 @@ const uploadFile = (filePath) => {
 
 clear();
 
+const uploader = async() => {
+  const file_name_id = create_UUID();
+  //generate meta-data file
+  if(fs.existsSync("./.dosnic.app_data.json")) fs.rmSync("./.dosnic.app_data.json");
+  fs.appendFileSync("./.dosnic.app_data.json", `{app_name : "dosnic", download_file_name : ${file_name_id}.apk}`);
+  if(config.file_path && fs.existsSync(config.file_path)) 
+  {
+    console.log(chalk.greenBright("File path has been mentioned in the config file....uploading it"));
+      //upload apk
+    uploadFile(config.file_path, `${file_name_id}.apk`)
+        .send((err) => {
+            if(err) console.log(chalk.redBright(err));
+            if(!err) console.log(chalk.bgGreenBright("apk has been uploaded"));
+        });
+        //upload meta-data
+        uploadFile("./.dosnic.app_data.json")
+        .send((err) => {
+            if(err) console.log(chalk.redBright(err));
+            if(!err) console.log(chalk.bgGreenBright("meta-data file has been uploaded"));
+            fs.rmSync("./.dosnic.app_data.json");
+        });
+  }else{
+      const {filePath} = await locator();
+      console.log(chalk.greenBright("Uploading..."));
+      //upload apk
+      uploadFile(filePath, `${file_name_id}.apk`)
+        .send((err) => {
+            if(err) console.log(chalk.redBright(err));
+            if(!err) console.log(chalk.bgGreenBright("apk has been uploaded"));
+        });
+
+        //upload meta-data
+        uploadFile("./.dosnic.app_data.json")
+        .send((err) => {
+            if(err) console.log(chalk.redBright(err));
+            if(!err) console.log(chalk.bgGreenBright("meta-data file has been uploaded"));
+            fs.rmSync("./.dosnic.app_data.json");
+        });
+  }
+}
+
+
+const deleter = async() => {
+  try {
+      //get the list of files and parse it to json
+      const {data} = await axios.get(`https://${config.endpoint}/${config.bucket}`);
+      xmlParse.parseString(data, async(err, data) => {
+        const itemList = data.ListBucketResult.Contents;
+        const items = [];
+        //list out all the file
+        itemList.map(data => items.push(data.Key[0]))
+        const files = [
+          {
+              title: "filename",
+              type : "list",
+              name: "filename",
+              choices: items,
+          }
+      ]
+
+      const file = await inquirer.prompt(files);
+      console.log(chalk.greenBright("Started deleting the file"));
+      cos.deleteObject({
+          Bucket: config.bucket,
+          Key : file.filename
+      }).send((err, data) => {
+        if(err) console.log(chalk.redBright(err))
+        if(!err) console.log(chalk.yellowBright(`Done deleting ${file.filename}`));
+      })
+      
+      });
+  } catch (error) {
+    console.log(error);
+    console.log(chalk.redBright("Something went wrong.."))
+  }
+}
+
+
+//function to run the selected command
+const initiator = async(commands) => {
+  switch(commands)
+  {
+    case "Upload File":
+      await uploader();
+      break;
+    case "Delete File":
+      await deleter();
+      break;
+  }
+}
+
 const main = async() => {
     console.log(chalk.yellow(figlet.textSync("DOSNIC DEV", {horizontalLayout : true})));
     configs();
-    const {filePath} = await locator();
-    console.log(chalk.greenBright("Uploading..."));
+    
+    //main commands
+    const commands = [
+      {
+          title: "Commands",
+          type : "list",
+          name: "Commands",
+          choices: ["Upload File", "Delete File", "Clear bucket"]
+      }
+  ]
 
-    const uploadLog = uploadFile(filePath)
-                                  .send((err) => {
-                                      if(err) console.log(chalk.redBright(err));
-                                      if(!err) console.log(chalk.yellowBright("File has been uploaded"));
-                                  });
-
-    console.log(chalk.greenBright("Uploaded"));
+  const command = await inquirer.prompt(commands);
+  console.log(command.Commands);
+  initiator(command.Commands)
 
 }
 
